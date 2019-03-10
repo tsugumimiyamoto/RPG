16a826bf0ce556cf0a35e32aead19a5948f110d7
tsugumimiyamoto <miyamoto@route-zero.com>
2019-03-10 14:58:47 +0900

    バトル追加

--- Character.java
+++ Character.java
@@ -1,11 +1,26 @@
 
 public class Character {
-	int hp;
-	int mp;
-	String name;
+	private String name; //名前
+	private int hp; //体力
+	int power; //攻撃力
+	private int mp; //魔力
 
+public Character(String name, int hp, int power, int mp) {
+	this.name=name;
+	this.hp=hp;
+	this.power=power;
+	this.mp=mp;
+}
 	public void introduce() {
-		System.out.println("わたしの名前は"+name+"！");
-		System.out.println("HPは"+hp+" / MPは"+mp+"だよ！！");
+		System.out.println("わたしの名前は"+this.name+"！");
+		System.out.println("HP "+this.hp+" / POWER "+this.power+" / MP "+this.mp+"だよ！！");
+	}
+	public void attack() {
+		System.out.println(this.name+"の攻撃！");
+	}
+	public void damage(int power) {
+		System.out.println(this.name+"は"+power+"のダメージを受けた！！");
+		this.hp=hp - power;
+		System.out.println(this.name+"のHPは"+this.hp+"になった。");
 	}
 }
--- RPG.java
+++ RPG.java
@@ -2,36 +2,21 @@
 public class RPG {
 	public static void main(String [] args) {
 
-		Character c;
-		c=new Character();
-		c.hp=100;
-		c.mp=90;
-		c.name="ミッキー";
+		//キャラクター＝名前、体力、攻撃力、魔力
+		Character c1=new Character("ミッキー",100,30,90);
+		Character c2=new Character("ミニー",80,20,50);
+		Character c3=new Character("ドナルド",90,40,80);
+		Character c4=new Character("アリエル",50,30,80);
 
-		Character c2;
-		c2=new Character();
-		c2.hp=80;
-		c2.mp=50;
-		c2.name="ミニー";
-
-		Character c3;
-		c3=new Character();
-		c3.hp=90;
-		c3.mp=80;
-		c3.name="ドナルド";
-
-		Character c4;
-		c4=new Character();
-		c4.hp=50;
-		c4.mp=80;
-		c4.name="アリエル";
-
-		c.introduce();
+		//自己紹介
+		c1.introduce();
 		c2.introduce();
 		c3.introduce();
 		c4.introduce();
 
-		System.out.println("さあ、だれと冒険する？");
-		System.out.println("この中からひとり選択しよう！");
+		System.out.println("ーーーーーーーバトル開始ーーーーーーーー");
+
+		c1.attack();
+		c3.damage(c1.power);
 	}
 }
